884814b7bb7ec8320fbaf3a6e2b93fe2b804a1fb
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-04-04 18:10:17 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/teste', function(req,res){
+    res.send('OK!')
+})
+
 app.listen(3001, function () {
     console.log('Servidor rodando na porta 3001');
 }
